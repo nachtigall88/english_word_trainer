1390e53ce930cc5d40d129dc29be001f7a6adc97
Max <udin@esmil.eu>
2023-07-20 12:28:59 +0300

    1

--- main_body/main_body.py
+++ main_body/main_body.py
@@ -3,6 +3,9 @@ class Word:
         self.word = word
         self.translation = translation
 
-
     def __str__(self):
-        return f'{self.word} {self.translation}'
+        return f'{self.word} {":"} {self.translation}'
+
+
+# w = Word('hello', 'привіт')
+# print(w)
