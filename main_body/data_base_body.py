a9294d3681ba3d94460f127631ea75771d0fb816
Max <udin@esmil.eu>
2023-07-21 09:17:36 +0300

    add db

--- main_body/data_base_body.py
+++ main_body/data_base_body.py
@@ -1,11 +1,17 @@
-import sqlite3
+import sqlite3 as sq
 
 class DataBase:
     def __init__(self):
-        pass
+        self.make_data_base()
 
     def make_data_base(self):
-        pass
+        with sq.connect('eng_word.db') as con:
+            cur = con.cursor()
+            cur.execute("""
+            """)
 
     def add_data(self):
         pass
+
+if __name__ == '__main__':
+    db = DataBase()
